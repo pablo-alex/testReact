55fa566c030a63c74d0f4f406732cb8e2626cf99
pabloxd <ilusionistaxd@gmail.com>
2020-11-27 02:12:49 -0400

    Added Basic Routing and Componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+
+import About from './components/About';
+import Home from './components/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>App Vacia Ok</h1>
-    </div>
+    <Router>
+      <div className="App">
+  
+        <div className="navbar navbar-dark bg-dark">
+          <Link to="/" className="btn btn-dark">
+            Home      
+          </Link>
+          <NavLink to="/about" className="btn btn-dark" activeClassName="active">
+            About
+          </NavLink>
+        </div>
+        <hr />
+
+        <Switch>
+          <Route path="/about"> 
+            <About />
+          </Route>
+          <Route path="/"> 
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>Estoy en About</h1>
+        </div>
+    )
+}
+
+export default About
